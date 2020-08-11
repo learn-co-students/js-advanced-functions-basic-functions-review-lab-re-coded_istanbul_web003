5f1110a614ccb236b9fe30c52a2c2849a5baab67
Hiba <hiba.mashfij@gmail.com>
2020-08-11 14:58:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+function saturdayFun(string = "roller-skate") {
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(whereTogo = "go to the office") {
+    return `This Monday, I will ${whereTogo}.`
+};
+
+
+function wrapAdjective(string = '*') {
+    return function (adj = "special") {
+        return `You are ${string}${adj}${string}!`
+    }
+};
+
+let Calculator = {
+    add: function (num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function (num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function (num1, num2) {
+        return num1 * num2;
+    },
+    divide: function (num1, num2) {
+        return num1 / num2;
+    },
+};
+
+
+function actionApplyer(num, array) {
+    if (array.length > 0) {
+        const one = array[0](num);
+        const two = array[1](one);
+        const three = array[2](two);
+
+        return three;
+    }
+
+    else {
+        return num;
+    }
+
+
+
+}
