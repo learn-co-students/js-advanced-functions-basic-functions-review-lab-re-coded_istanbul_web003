69f3624b4b67e3739b5c28465d67dc547997e9cb
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-10-22 12:52:27 +0300

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,35 @@ function mondayWork(act='go to the office'){
 }
 mondayWork();
 
-function wrapAdjective () {
-    return function inner (x='special') {
-        return `You are ${x}`;
+function wrapAdjective(par="*"){
+    return function(where="special"){
+          return`You are ${par}${where}${par}!`
+        }
+}
+
+let Calculator={
+    add: function(num1="1",num2="3") {
+        return num1 + num2;
+    },
+    subtract: function(num1="1",num2="3"){
+        return num1 - num2;
+    },
+    multiply: function(num1="1",num2="3"){
+        return num1 * num2;
+    },
+    divide: function(num1="1",num2="3"){
+        return num1 / num2;
     }
+};
+    
+function actionApplyer(int,arr){
+    let result;
+    for(let i=0;i<arr.length;i++){
+        result= arr[i](int);
+        int = result;
+    }
+    if(arr.length===0){
+        return int;
+    }
+    return result;
 }
